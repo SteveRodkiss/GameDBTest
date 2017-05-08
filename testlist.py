b08099c042d479035c841a4f0992318d38960a2b
SteveRodkiss <rds@burnside.school.nz>
2017-05-08 21:51:51 +1200

    fixed conficts

--- testlist.py
+++ testlist.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def printlist(alist):
     for item in alist:
         print(item)
@@ -6,12 +5,3 @@ def printlist(alist):
 
 things = ["onething","twothings","threethings"]
 printlist(things)
-=======
-def printlist(alist):
-    for item in alist:
-        print(item)
-
-
-things = ["onething","twothings","threethings"]
-printlist(things)
->>>>>>> fb58f84bf756a3944ef10767f5e71941983b03eb
